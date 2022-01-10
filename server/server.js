b81c40e544e5c073d13fda39b1180c239c65c79b
Casper Yonel <yonelcasper@gmail.com>
2022-01-10 18:25:05 -0500

    working on server 3000

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = process.env || 3000
+const {SERVER_PORT} = process.env
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
+app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
